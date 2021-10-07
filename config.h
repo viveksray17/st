849161efc34c7996ab182f7879e1b2e88ec68fce
Vivek Sundaray <viveksundaray123@outlook.com>
2021-10-07 08:18:57 +0530

    Done Some changes

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack:size=12:antialias=true:autohint=true";
+static char *font = "JetBrainsMono-Medium:size=12:antialias=true:autohint=true";
 static char *font2[] = { 
     "JoyPixels:size=12:antialias=true:autohint=true",
 };
